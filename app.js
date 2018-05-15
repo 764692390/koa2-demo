de69b967243a24fb88ed28f2a0f1f7d500cdff68
lizhi <lizhilz88_v@didichuxing.com>
2018-05-15 14:37:17 +0800

    修改session

--- app.js
+++ app.js
@@ -4,34 +4,30 @@ import bodyParser from 'koa-bodyparser'
 import views from 'koa-views'
 import koaStatic from 'koa-static'
 import cors from '@koa/cors'
-import KoaSessionredis from 'koa-session-redis'
+import session from 'koa-generic-session'
+import redisStore from 'koa-redis'
 import Redis from 'ioredis'
-
-
-const redis=new Redis(config.Redis);
-
 import router from './app/router';
 import config from './app/config';
 import log from './app/middlewares/log';
 
 const app = new Koa();
-
-/*设置session*/
-app.keys = ["some secret hurr"];
-
+const redis=new Redis(config.Redis);
+app.keys = ['keys', 'keykeys'];
 app
+    .use(log())
+    .use(cors())
+    .use(bodyParser())
     .use( async(ctx,next) =>{
         ctx.redis = redis;
         await next();  
     })
-    .use(
-        KoaSessionredis({
-            store: config.SessionRedis
+    .use(session({
+        key: "LzShop",
+        store: redisStore({
+
         })
-    )
-    .use(log())
-    .use(cors())
-    .use(bodyParser())
+    }))
     .use(koaStatic(__dirname + "/app/public"))
     .use(
         views(__dirname + "/app/views", {
--- app/config/index.js
+++ app/config/index.js
@@ -1,10 +1,12 @@
+const IS_PRODUCTION = process.env.NODE_ENV === 'production';
+
 export default {
     port: process.env.PORT || 5000,
     //  基本信息配置
     base: {
         version: '1.0.0',
-        host: 'https://shop.jczxw.cn',
-        cdn: 'https://shop.jczxw.cn',
+        host: IS_PRODUCTION ? 'https://shop.jczxw.cn' : 'http://127.0.0.1:5000',
+        cdn: IS_PRODUCTION ? 'https://shop.jczxw.cn' : 'http://127.0.0.1:5000',
         keywords: "Egg,nodejs, node, express,egg,koa2,ThinkJS, socket.io,关注Web前端开发技术",
         description: "Egg,nodejs, node, express,egg,koa2,ThinkJS, socket.io",
         title: "LzShop",
