b14377eed269246bcb47c6ebe3f3a97a0f04fcdc
lizhi <lizhilz88_v@didichuxing.com>
2018-05-16 18:23:31 +0800

    修改版本号

--- app/config/index.js
+++ app/config/index.js
@@ -4,11 +4,11 @@ export default {
     port: process.env.PORT || 5000,
     //  基本信息配置
     base: {
-        version: '1.0.1',
+        version: '0.0.2',
         host: IS_PRODUCTION ? 'https://shop.jczxw.cn' : 'http://127.0.0.1:5000',
         cdn: IS_PRODUCTION ? 'https://shop.jczxw.cn' : 'http://127.0.0.1:5000',
-        keywords: "Egg,nodejs, node, express,egg,koa2,ThinkJS, socket.io,关注Web前端开发技术",
-        description: "Egg,nodejs, node, express,egg,koa2,ThinkJS, socket.io",
+        keywords: "Web前端开发",
+        description: "LzShop, Web前端开发",
         title: "LzShop",
         clas: '0',
     },
